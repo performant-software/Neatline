5ce42418f5b1b8d10bd6ea16f5988ae92970aced
David McClure <david.mcclure@virginia.edu>
2011-09-01 09:46:43 -0400

    css for menu done

--- views/admin/index/_header.php
+++ views/admin/index/_header.php
@@ -1,9 +1,13 @@
-<img id="neatline-logo" src="<?php echo img('neatline-logo-rgb-small.png'); ?>" />
+<div id="neatline-header">
 
-<ul id="section-nav" class="navigation">
-<?php echo nav(array(
-    'Neatlines' => uri(''),
-    'Maps' => uri(''),
-    'Timelines' => uri('')
-))?>
-</ul>
+    <img id="neatline-logo" src="<?php echo img('neatline-logo-rgb-small.png'); ?>" />
+
+    <ul id="section-nav" class="navigation">
+        <?php echo nav(array(
+            'Neatlines' => uri('neatline-exhibits'),
+            'Maps' => uri('neatline-exhibits/maps'),
+            'Timelines' => uri('neatline-exhibits/timelines')
+        ))?>
+    </ul>
+
+</div>
