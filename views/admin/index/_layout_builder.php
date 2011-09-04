10ecd1a7ef39112857cbc6acd9b4e06279c31c95
David McClure <david.mcclure@virginia.edu>
2011-09-04 16:46:38 -0400

    tinkering with js

--- views/admin/index/_layout_builder.php
+++ views/admin/index/_layout_builder.php
@@ -28,9 +28,17 @@
 <div id="layout-builder">
 
     <div id="options">
+
+        <!--
+        <span class="neatline-button" id="toggle-map">Map</span>
+        <span class="neatline-button" id="toggle-timeline">Timeline</span>
+        <span class="neatline-button" id="toggle-undated-items">Undated Items</span>
+        -->
+
         <input type="checkbox" id="toggle_map" /><label for="toggle_map">Map</label>
         <input type="checkbox" id="toggle_timeline" /><label for="toggle_timeline">Timeline</label>
         <input type="checkbox" id="toggle_undated_items" /><label for="toggle_undated_items">Undated Items</label>
+
     </div>
 
     <div id="drag-box"></div>
