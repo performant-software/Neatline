fe21011653bb879945f054f4772f9135d0f077f7
David McClure <david.mcclure@virginia.edu>
2011-09-17 17:39:29 -0400

    rewrote scrollbar width getter code

--- views/admin/javascripts/editor/item_browser.js
+++ views/admin/javascripts/editor/item_browser.js
@@ -356,25 +356,44 @@
 
             if ($.browser.msie) {
 
-				var $textarea1 = $('<textarea cols="10" rows="2"></textarea>')
-						.css({ position: 'absolute', top: -1000, left: -1000 }).appendTo('body'),
-					$textarea2 = $('<textarea cols="10" rows="2" style="overflow: hidden;"></textarea>')
-						.css({ position: 'absolute', top: -1000, left: -1000 }).appendTo('body');
+                var textarea1 = $('<textarea cols="10" rows="2"></textarea>')
+                    .css({
+                        position: 'absolute',
+                        top: -1000,
+                        left: -1000
+                    }).appendTo('body');
 
-				this.scrollbarWidth = $textarea1.width() - $textarea2.width();
-				$textarea1.add($textarea2).remove();
+                var textarea2 = $('<textarea cols="10" rows="2"></textarea>')
+                    .css({
+                        position: 'absolute',
+                        top: -1000,
+                        left: -1000,
+                        overflow: 'hidden'
+                    }).appendTo('body');
+
+                this.scrollbarWidth = textarea1.width() - textarea2.width();
+                textarea1.remove();
+                textarea2.remove();
 
 			}
 
             else {
 
-				var $div = $('<div />')
-					.css({ width: 100, height: 100, overflow: 'auto', position: 'absolute', top: -1000, left: -1000 })
-					.prependTo('body').append('<div />').find('div')
-						.css({ width: '100%', height: 200 });
+                var div = $('<div />')
+                    .css({
+                        width: 100,
+                        height: 100,
+                        overflow: 'auto',
+                        position: 'absolute',
+                        top: -1000,
+                        left: -1000
+                    }).prependTo('body').append('<div />').find('div').css({
+                        width: '100%',
+                        height: 200
+                    });
 
-				this.scrollbarWidth = 100 - $div.width();
-				$div.parent().remove();
+				this.scrollbarWidth = 100 - div.width();
+				div.parent().remove();
 
 			}
 
