d5dc874441db666e174a3411f29b7720e8e191d1
David McClure <david.mcclure@virginia.edu>
2011-12-02 10:38:37 -0500

    shell for exhibit row tests

---
+++ tests/unit/NeatlineNeatlineTest.php
@@ -0,0 +1,48 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * Exhibit row tests.
+ *
+ * PHP version 5
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @author      Scholars' Lab <>
+ * @author      Bethany Nowviskie <bethany@virginia.edu>
+ * @author      Adam Soroka <ajs6f@virginia.edu>
+ * @author      David McClure <david.mcclure@virginia.edu>
+ * @copyright   2011 The Board and Visitors of the University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+ */
+?>
+
+<?php
+
+class Neatline_NeatlineExhibitTest extends Omeka_Test_AppTestCase
+{
+
+    /**
+     * Instantiate the helper class, install the plugins, get the database.
+     *
+     * @return void.
+     */
+    public function setUp()
+    {
+
+        parent::setUp();
+        $this->helper = new Neatline_Test_AppTestCase;
+        $this->helper->setUpPlugin();
+        $this->db = get_db();
+
+    }
+
+}
