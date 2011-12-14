cc6bcdef56df50a4e119ad252ab4accb48198b2b
David McClure <david.mcclure@virginia.edu>
2011-12-14 13:43:43 -0500

    [Untitled] default titles

--- views/admin/editor/items-ajax.php
+++ views/admin/editor/items-ajax.php
@@ -39,7 +39,7 @@
 
             <tr class="item-row" recordid="<?php echo $record->id; ?>">
                 <td class="item-title">
-                    <span class="item-title-text"><?php echo $record->title; ?></span>
+                    <span class="item-title-text"><?php echo ($record->title != null) ? $record->title : '[Untitled]'; ?></span>
                     <span class="item-title-fader"></span>
                 </td>
                 <td class="col-1 col-row space">
