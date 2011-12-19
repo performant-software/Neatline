54b6df62baa412b804f6f094b1e7341e27966e58
David McClure <david.mcclure@virginia.edu>
2011-12-19 17:02:15 -0500

    fixes show callback in layout manager

--- views/admin/editor/_topbar_navigation.php
+++ views/admin/editor/_topbar_navigation.php
@@ -28,7 +28,7 @@
 <ul class="nav">
 
     <li class="dropdown">
-        <a href="" id="configure-layout-button" class="dropdown-toggle">Layout</a>
+        <a href="" id="configure-layout-button" class="dropdown-toggle">Configure Layout</a>
         <div id="configure-layout" class="dropdown-content">
             <?php echo $this->partial('editor/_configure_layout.php'); ?>
         </div>
