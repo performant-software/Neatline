ebc4c85d53cf73264dfa37c82c6b7fa815198b6b
David McClure <david.mcclure@virginia.edu>
2011-12-20 09:14:51 -0500

    measure() test in positioner

--- spec/javascripts/PositionerSpec.js
+++ spec/javascripts/PositionerSpec.js
@@ -45,12 +45,15 @@ describe('Positioner', function() {
         });
 
         it('should select the markup for the component blocks', function() {
+
             expect(neatline.positioner('getAttr', 'map')).toBe('#test-map');
             expect(neatline.positioner('getAttr', 'timeline')).toBe('#test-timeline');
             expect(neatline.positioner('getAttr', 'items')).toBe('#test-items');
+
         });
 
         it('should capture starting parameter defaults', function() {
+
             expect(neatline.positioner('getAttr', '_is_map')).toBeFalsy();
             expect(neatline.positioner('getAttr', '_is_timeline')).toBeFalsy();
             expect(neatline.positioner('getAttr', '_is_items')).toBeFalsy();
@@ -58,9 +61,11 @@ describe('Positioner', function() {
             expect(neatline.positioner('getAttr', '_items_v_pos')).toEqual('bottom');
             expect(neatline.positioner('getAttr', '_items_h_pos')).toEqual('right');
             expect(neatline.positioner('getAttr', '_items_height')).toEqual('full');
+
         });
 
         it('should shell out the positions object', function() {
+
             expect(neatline.positioner('getAttr', 'positions')).toEqual({
                 map: {
                     height: null,
@@ -81,15 +86,82 @@ describe('Positioner', function() {
                     left:   null
                 }
             });
+
         });
 
         it('should call the measure() method by default', function() {
+
             expect(neatline.positioner('getAttr', 'width')).toBeDefined();
             expect(neatline.positioner('getAttr', 'height')).toBeDefined();
             expect(neatline.positioner('getAttr', 'minorWidth')).toBeDefined();
             expect(neatline.positioner('getAttr', 'majorWidth')).toBeDefined();
             expect(neatline.positioner('getAttr', 'minorHeight')).toBeDefined();
             expect(neatline.positioner('getAttr', 'majorHeight')).toBeDefined();
+
+        });
+
+    });
+
+    describe('measure', function() {
+
+        var neatline;
+
+        beforeEach(function() {
+
+            // Get exhibit markup.
+            loadFixtures('neatline.html');
+
+            // Get container and set dimensions.
+            neatline = $('#neatline');
+            neatline.css({
+                'width': 100,
+                'height': 100
+            });
+
+            // Roll up the positioner.
+            neatline.positioner({
+                markup: {
+                    map:            '#test-map',
+                    timeline:       '#test-timeline',
+                    items:          '#test-items'
+                },
+                constants: {
+                    h_percent:      30,
+                    v_percent:      60
+                }
+            });
+
+        });
+
+        it('should compute constants based on options at startup', function() {
+
+            expect(neatline.positioner('getAttr', 'width')).toEqual(100);
+            expect(neatline.positioner('getAttr', 'height')).toEqual(100);
+            expect(neatline.positioner('getAttr', 'minorWidth')).toEqual(30);
+            expect(neatline.positioner('getAttr', 'majorWidth')).toEqual(70);
+            expect(neatline.positioner('getAttr', 'minorHeight')).toEqual(40);
+            expect(neatline.positioner('getAttr', 'majorHeight')).toEqual(60);
+
+        });
+
+        it('should compute new constants correctly after a container resize', function() {
+
+            // Change the size of the container.
+            neatline.css({
+                'width': 200,
+                'height': 200
+            });
+
+            // Re-measure.
+            neatline.positioner('measure');
+
+            expect(neatline.positioner('getAttr', 'width')).toEqual(200);
+            expect(neatline.positioner('getAttr', 'height')).toEqual(200);
+            expect(neatline.positioner('getAttr', 'minorWidth')).toEqual(60);
+            expect(neatline.positioner('getAttr', 'majorWidth')).toEqual(140);
+            expect(neatline.positioner('getAttr', 'minorHeight')).toEqual(80);
+            expect(neatline.positioner('getAttr', 'majorHeight')).toEqual(120);
+
         });
 
     });
