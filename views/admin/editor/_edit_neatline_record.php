04138f337dbd1852916f596604b8efa0df7d4f74
David McClure <david.mcclure@virginia.edu>
2011-12-29 10:39:58 -0500

    gets rid of old data filler calls in form template

--- views/admin/editor/_edit_neatline_record.php
+++ views/admin/editor/_edit_neatline_record.php
@@ -36,7 +36,7 @@
             <div class="clearfix">
                 <label for="title">Title</label>
                 <div class="input">
-                    <input class="xlarge" name="title" size="30" placeholder="Title" type="text" value="<?php echo $neatline->getTextByItemAndField($item, 'Title'); ?>" />
+                    <input class="xlarge" name="title" size="30" placeholder="Title" type="text" />
                     <span class="help-block">The text displayed as the item's icon on the timeline.</span>
                 </div>
             </div>
@@ -44,7 +44,7 @@
             <div class="clearfix">
                 <label for="map-description">Description</label>
                 <div class="input">
-                    <textarea class="xlarge" name="description" placeholder="Description" rows="4"><?php echo $neatline->getTextByItemAndField($item, 'Description'); ?></textarea>
+                    <textarea class="xlarge" name="description" placeholder="Description" rows="4"></textarea>
                     <span class="help-block">Descriptive text associated with the item's timeline entry.</span>
                 </div>
             </div>
@@ -53,8 +53,8 @@
                 <label>Start Date</label>
                 <div class="input">
                     <div class="inline-inputs">
-                        <input class="medium" name="start-date-date" type="text" placeholder="Date" value="<?php echo $neatline->getTimeTextByItemAndField($item, 'start_date'); ?>">
-                        <input class="mini" name="start-date-time" type="text" placeholder="Time" value="<?php echo $neatline->getTimeTextByItemAndField($item, 'start_time'); ?>">
+                        <input class="medium" name="start-date-date" type="text" placeholder="Date" />
+                        <input class="mini" name="start-date-time" type="text" placeholder="Time" />
                         <span class="help-inline">Enter the date (or start date) of the item.</span>
                     </div>
                 </div>
@@ -64,8 +64,8 @@
                 <label>End Date</label>
                 <div class="input">
                     <div class="inline-inputs">
-                        <input class="medium" name="end-date-date" type="text" placeholder="Date" value="<?php echo $neatline->getTimeTextByItemAndField($item, 'end_date'); ?>">
-                        <input class="mini" name="end-date-time" type="text" placeholder="Time" value="<?php echo $neatline->getTimeTextByItemAndField($item, 'end_time'); ?>">
+                        <input class="medium" name="end-date-date" type="text" placeholder="Date" />
+                        <input class="mini" name="end-date-time" type="text" placeholder="Time" />
                         <span class="help-inline">Enter an ending date, if applicable.</span>
                     </div>
                 </div>
@@ -80,22 +80,23 @@
                                 <div class="stop-marker left"><div class="color-swatch"></div></div>
                                 <div class="stop-marker right"><div class="color-swatch"></div></div>
                             </div>
-                            <input name="left-ambiguity-percentage" type="hidden" value="<?php echo $neatline->getAmbiguityPercentage($item, 'left'); ?>">
-                            <input name="right-ambiguity-percentage" type="hidden" value="<?php echo $neatline->getAmbiguityPercentage($item, 'right'); ?>">
+                            <input name="left-ambiguity-percentage" type="hidden" />
+                            <input name="right-ambiguity-percentage" type="hidden" />
                         </div>
-                        <span class="help-inline">Drag the beginning and ending sliders inward to capture uncertainty over the date interval.</span>
+                        <span class="help-inline">Drag the sliders to represent
+                            uncertainty over the date interval.</span>
                     </div>
                 </div>
             </div>
 
             <div class="clearfix">
                 <label>Shape Color</label>
-                <input type="text" id="color-<?php echo $item->id; ?>" class="color-picker" name="color" value="<?php echo $neatline->getTextByItemAndField($item, 'Identifier'); ?>" />
+                <input type="text" id="color-<?php echo $item->id; ?>" class="color-picker" name="color" />
                 <span class="help-inline">Select a color for the item's spatial vectors.</span>
             </div>
 
             <div class="actions">
-                <input type="submit" class="btn primary" value="Save">&nbsp;<button type="reset" class="btn">Close</button>
+                <input type="submit" class="btn primary" value="Save">
             </div>
 
         </fieldset>
