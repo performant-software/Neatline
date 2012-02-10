fe839211578b64cf40a95c6927bbaf87993952aa
David McClure <david.mcclure@virginia.edu>
2012-02-10 15:29:20 -0500

    indexOf again

--- views/shared/javascripts/libraries/_utilities.js
+++ views/shared/javascripts/libraries/_utilities.js
@@ -48,13 +48,11 @@ Array.prototype.contains = function(el) {
 };
 
 // Manually define indexOf (missing in IE8-)
-if (!Array.indexOf) {
-    Array.prototype.indexOf = function(obj) {
-        for (var i=0; i<this.length; i++) {
-            if (this[i]==obj) {
-                return i;
-            }
+Array.prototype.indexOf = function(obj) {
+    for (var i=0; i<this.length; i++) {
+        if (this[i]==obj) {
+            return i;
         }
-        return -1;
-    };
-}
+    }
+    return -1;
+};
