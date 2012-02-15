efeb79b6013a1b294a1c0853d9c9536d468917db
David McClure <david.mcclure@virginia.edu>
2012-02-15 09:03:37 -0500

    shell for item browser tests

--- spec/javascripts/unit/ItemBrowserSpec.js
+++ spec/javascripts/unit/ItemBrowserSpec.js
@@ -1,21 +1,60 @@
 /*
- * Unit tests for the Neatline positioning manager.
+ * Unit tests for the items block.
  */
 
 describe('Item Browser', function() {
 
-    var browser;
+    var browser, request;
+
+    // Mock _getItems() response.
+    itemsResponse = {
+        status: 200,
+        responseText: readFixtures('editor-items-ajax.html')
+    };
 
     beforeEach(function() {
 
-        // Get exhibit markup.
+        // Get editor markup.
         loadFixtures('editor.html');
 
         // Get container and set dimensions.
         browser = $('#item-browser');
 
-        // Run the browser.
-        browser.itembrowser();
+        // Install AJAX mock.
+        jasmine.Ajax.useMock();
+
+        // Create spies for events.
+        // itemClick = jasmine.createSpy('itemclick');
+        // itemEnter = jasmine.createSpy('itementer');
+        // itemLeave = jasmine.createSpy('itemleave');
+
+        // Run the widget and wire up event spies.
+        // items.neatlineitems({
+        //     'itemclick': itemClick,
+        //     'itementer': itemEnter,
+        //     'itemleave': itemLeave
+        // });
+        itemBrowser.itembrowser();
+
+        // Capture the request and set response.
+        request = mostRecentAjaxRequest();
+        request.response(itemsResponse);
+
+    });
+
+    describe('_create', function() {
+
+        it('should load items', function() {
+
+            // Check for markup presence.
+            // expect(container).not.toBeEmpty();
+            // expect(container).toContain($('ul'));
+            // expect(container).toContain($('ul li.item-title[recordid="1"]'));
+            // expect(container).toContain($('ul li.item-title[recordid="2"]'));
+            // expect(container).toContain($('ul li.item-title[recordid="3"]'));
+            // expect(container).toContain($('ul li.item-title[recordid="4"]'));
+
+        });
 
     });
 
