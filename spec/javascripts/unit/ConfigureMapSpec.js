10b0e6853bbe13d057b2506f5f9bbab92f1be04a
David McClure <david.mcclure@virginia.edu>
2012-02-16 14:20:57 -0500

    stub for configure map test

---
+++ spec/javascripts/unit/ConfigureMapSpec.js
@@ -0,0 +1,26 @@
+/*
+ * Unit tests for the map configuration dropdown.
+ */
+
+describe('Item Form', function() {
+
+    beforeEach(function() {
+
+        // Get editor markup.
+        loadFixtures('editor.html');
+
+        // Get container and set dimensions.
+        browser = $('#item-browser');
+        form = $('#edit-form');
+
+    });
+
+    afterEach(function() {
+
+    });
+
+    describe('stub', function() {
+
+    });
+
+});
