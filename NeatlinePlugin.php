80c8b54c82c0dbbccef48ae79238e7db87823a4d
David McClure <david.mcclure@virginia.edu>
2012-05-20 12:16:16 -0400

    gets rid of old upgrade in plugin.php

--- NeatlinePlugin.php
+++ NeatlinePlugin.php
@@ -210,59 +210,6 @@ class NeatlinePlugin
     public function upgrade($oldVersion, $newVersion)
     {
 
-        if (version_compare($oldVersion, '0.1', '<=')) {
-
-            // `modified` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_exhibits`
-                ADD COLUMN `modified` TIMESTAMP NULL";
-            $this->_db->query($sql);
-
-            // `h_percent` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_exhibits`
-                ADD COLUMN `h_percent` int(10) unsigned NULL";
-            $this->_db->query($sql);
-
-            // `v_percent` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_exhibits`
-                ADD COLUMN `v_percent` int(10) unsigned NULL";
-            $this->_db->query($sql);
-
-            // `default_timeline_zoom` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_exhibits`
-                ADD COLUMN `default_timeline_zoom` int(10) unsigned NULL";
-            $this->_db->query($sql);
-
-            // `default_highlight_color` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_exhibits`
-                ADD COLUMN `default_highlight_color` tinytext COLLATE utf8_unicode_ci NULL";
-            $this->_db->query($sql);
-
-            // `default_base_layer` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_exhibits`
-                ADD COLUMN `default_base_layer` int(10) unsigned NULL";
-            $this->_db->query($sql);
-
-            // `highlight_color` column.
-            $sql = "ALTER TABLE `{$this->_db->prefix}neatline_data_records`
-                ADD COLUMN `highlight_color` tinytext COLLATE utf8_unicode_ci NULL";
-            $this->_db->query($sql);
-
-            // Create layers table.
-            $sql = "CREATE TABLE IF NOT EXISTS `{$this->_db->prefix}neatline_base_layers` (
-                    `id`                    int(10) unsigned not null auto_increment,
-                    `name`                  tinytext COLLATE utf8_unicode_ci NULL,
-                     PRIMARY KEY (`id`)
-                   ) ENGINE=innodb DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
-            $this->_db->query($sql);
-
-            // Set default map style attributes.
-            neatline_setMapStyleDefaults();
-
-            // Install base layers.
-            neatline_installBaseLayers();
-
-        }
-
     }
 
     /**
@@ -335,10 +282,8 @@ class NeatlinePlugin
      */
     public function defineRoutes($router)
     {
-
         $router->addConfig(new Zend_Config_Ini(NEATLINE_PLUGIN_DIR
             .'/routes.ini', 'routes'));
-
     }
 
     /**
@@ -348,9 +293,7 @@ class NeatlinePlugin
      */
     public function adminAppendToPluginUninstallMessage()
     {
-
         echo neatline_uninstallWarningMessage();
-
     }
 
     /**
