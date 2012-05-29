3d1614e879a72ddb8902f7cf80f63cae41d45a15
David McClure <david.mcclure@virginia.edu>
2012-05-29 08:35:28 -0400

    adds link to item show page in DC-backed bubbles

--- views/shared/neatline/_dc_metadata.php
+++ views/shared/neatline/_dc_metadata.php
@@ -1,13 +1,18 @@
-    <?php echo show_item_metadata(array(), $item); ?>
+<?php echo link_to_item('Open item home page in new tab', array(
+    'class' => 'item-dc-link',
+    'target' => '_blank'
+), 'show', $item); ?>
 
-    <!-- The following returns all of the files associated with an item. -->
-    <div id="itemfiles" class="element">
-        <h3><?php echo __('Files'); ?></h3>
-        <div class="element-text"><?php echo display_files_for_item(array(), array(), $item); ?></div>
-    </div>
+<?php echo show_item_metadata(array(), $item); ?>
 
-    <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo item_citation($item); ?></div>
-    </div>
+<!-- The following returns all of the files associated with an item. -->
+<div id="itemfiles" class="element">
+    <h3><?php echo __('Files'); ?></h3>
+    <div class="element-text"><?php echo display_files_for_item(array(), array(), $item); ?></div>
+</div>
+
+<!-- The following prints a citation for this item. -->
+<div id="item-citation" class="element">
+    <h3><?php echo __('Citation'); ?></h3>
+    <div class="element-text"><?php echo item_citation($item); ?></div>
+</div>
