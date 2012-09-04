2af1dfae691eef85f85730987e73f84d3802e6f7
David McClure <david.mcclure@virginia.edu>
2012-09-04 07:46:58 -0400

    formatting changes in plugin.php

--- plugin.php
+++ plugin.php
@@ -2,17 +2,7 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
 
 /**
- * Ignition file.
- *
- * PHP version 5
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
- * applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, either express or implied. See the License for the specific
- * language governing permissions and limitations under the License.
+ * Plugin runner.
  *
  * @package     omeka
  * @subpackage  neatline
@@ -20,13 +10,12 @@
  * @author      Bethany Nowviskie <bethany@virginia.edu>
  * @author      Adam Soroka <ajs6f@virginia.edu>
  * @author      David McClure <david.mcclure@virginia.edu>
- * @copyright   2011 Rector and Board of Visitors, University of Virginia
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
  */
 
 
 // constants {{{
-
 if (!defined('NEATLINE_PLUGIN_VERSION')) {
     define('NEATLINE_PLUGIN_VERSION', get_plugin_ini('Neatline', 'version'));
 }
@@ -34,20 +23,12 @@ if (!defined('NEATLINE_PLUGIN_VERSION')) {
 if (!defined('NEATLINE_PLUGIN_DIR')) {
     define('NEATLINE_PLUGIN_DIR', dirname(__FILE__));
 }
-
 // }}}
 
 
 // requires {{{
-
 require_once NEATLINE_PLUGIN_DIR . '/NeatlinePlugin.php';
-require_once NEATLINE_PLUGIN_DIR . '/helpers/NeatlineFunctions.php';
-require_once NEATLINE_PLUGIN_DIR . '/helpers/NeatlineRenderer.php';
-require_once NEATLINE_PLUGIN_DIR . '/forms/NeatlineDetails.php';
 require_once HELPERS;
-
-
 // }}}
 
-// Instantiate the manager class.
 new NeatlinePlugin;
