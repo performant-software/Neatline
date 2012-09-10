4d0efe1736eee8679daa2536a2e7eef6b4bd0dc5
David McClure <david.mcclure@virginia.edu>
2012-09-10 09:27:09 -0400

    shell for data controller

---
+++ controllers/DataController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Data emitter.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class Neatline_IndexController extends Omeka_Controller_AbstractActionController
+{
+
+}
