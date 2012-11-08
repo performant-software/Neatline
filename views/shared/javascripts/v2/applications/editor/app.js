c992ab2eb0fb2eec5e7b28f645857be177f1df8a
David McClure <david.mcclure@virginia.edu>
2012-11-08 12:03:20 -0500

    Shell for editor application.

---
+++ views/shared/javascripts/v2/applications/editor/app.js
@@ -0,0 +1,17 @@
+/**
+ * Neatline editor.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+NeatlineEditor = new Backbone.Marionette.Application();
+
+// --------------------
+// Instance namespaces.
+// --------------------
+NeatlineEditor.Controllers = {};
+NeatlineEditor.Collections = {};
+NeatlineEditor.Views = {};
---
+++ views/shared/javascripts/v2/applications/editor/run.js
@@ -0,0 +1,10 @@
+/**
+ * Start the Neatline application.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+$(function() { NeatlineEditor.start(); });
