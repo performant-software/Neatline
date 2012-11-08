9e35fae5b840eda040073675a214a4b861f2bdb2
David McClure <david.mcclure@virginia.edu>
2012-11-08 10:02:25 -0500

    Click/hover actions wired up on features.

--- views/shared/javascripts/v2/applications/neatline/views/map-view.js
+++ views/shared/javascripts/v2/applications/neatline/views/map-view.js
@@ -15,7 +15,13 @@ Neatline.Views.Map = Backbone.View.extend({
    * @return void.
    */
   initialize: function() {
+
+    // Trackers.
+    this.layers = [];
+
+    // Instantiate OpenLayers.
     this.initializeOpenLayers();
+
   },
 
   /*
@@ -83,7 +89,20 @@ Neatline.Views.Map = Backbone.View.extend({
    * @return void.
    */
   ingest: function(records) {
+    this.buildLayers(records);
+    this.addCursorControls();
+  },
 
+  /*
+   * Ingest records.
+   *
+   * @param {Object} records: The records collection.
+   *
+   * @return void.
+   */
+  buildLayers: function(records) {
+
+    // Add records to map.
     records.each(_.bind(function(record) {
 
       // If active on the map.
@@ -97,13 +116,89 @@ Neatline.Views.Map = Backbone.View.extend({
         var layer = new OpenLayers.Layer.Vector(record.get('title'));
         layer.addFeatures(geometry);
 
-        // Add to map.
+        // Add to map, track.
         this.map.addLayer(layer);
+        this.layers.push(layer);
 
       }
 
     }, this));
 
+  },
+
+  /*
+   * Listen for hover and click on geometries.
+   *
+   * @return void.
+   */
+  addCursorControls: function() {
+
+    // Build the hover control, bind callbacks.
+    this.hoverControl = new OpenLayers.Control.SelectFeature(
+      this.layers, {
+        hover: true,
+        overFeature: this.onFeatureHighlight,
+        outFeature: this.onFeatureUnhighlight
+      }
+    );
+
+    // Build the click control, bind callbacks.
+    this.clickControl = new OpenLayers.Control.SelectFeature(
+      this.layers, {
+        onSelect: this.onFeatureSelect,
+        onUnselect: this.onFeatureUnselect
+      }
+    );
+
+    // Add to map, activate.
+    this.map.addControls([this.hoverControl, this.clickControl]);
+    this.hoverControl.activate();
+    this.clickControl.activate();
+
+  },
+
+  /*
+   * When a feature is selected.
+   *
+   * @param {Object|OpenLayers.Feature} feature: The feature.
+   *
+   * @return void.
+   */
+  onFeatureSelect: function(feature) {
+    console.log('select');
+  },
+
+  /*
+   * When a feature is unselected.
+   *
+   * @param {Object|OpenLayers.Feature} feature: The feature.
+   *
+   * @return void.
+   */
+  onFeatureUnselect: function(feature) {
+    console.log('unselect');
+  },
+
+  /*
+   * When a feature is highlighted.
+   *
+   * @param {Object|OpenLayers.Feature} feature: The feature.
+   *
+   * @return void.
+   */
+  onFeatureHighlight: function(feature) {
+    console.log('highlight');
+  },
+
+  /*
+   * When a feature is un-highlighted.
+   *
+   * @param {Object|OpenLayers.Feature} feature: The feature.
+   *
+   * @return void.
+   */
+  onFeatureUnhighlight: function(feature) {
+    console.log('unhighlight');
   }
 
 });
