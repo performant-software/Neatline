f5417d8e2cfaf2380720641a23c3ac34e368cb4d
David McClure <david.mcclure@virginia.edu>
2012-11-14 09:32:38 -0500

    Test for editor /records.

--- tests/integration/EditorControllerTest.php
+++ tests/integration/EditorControllerTest.php
@@ -14,15 +14,48 @@
 class Neatline_EditorControllerTest extends Neatline_Test_AppTestCase
 {
 
-    protected $_isAdminTest = true;
-
     /**
-     * .
+     * /records should emit a JSON object containing all record data
+     * needed by the editor.
      *
      * @return void.
      */
     public function testRecords()
     {
+
+        // Create exhibit and record.
+        $exhibit = $this->__exhibit();
+        $record1 = $this->__record(null, $exhibit);
+        $record2 = $this->__record(null, $exhibit);
+
+        // Hit data route.
+        $this->dispatch('neatline/editor/records/'.$exhibit->id);
+        $response = json_decode($this->getResponse()->getBody('default'));
+
+        // Check code and length.
+        $this->assertResponseCode(200);
+        $this->assertEquals(count($response), 2);
+
+        // Check for keys.
+        $this->assertObjectHasAttribute('id',                 $response[0]);
+        $this->assertObjectHasAttribute('item_id',            $response[0]);
+        $this->assertObjectHasAttribute('title',              $response[0]);
+        $this->assertObjectHasAttribute('description',        $response[0]);
+        $this->assertObjectHasAttribute('slug',               $response[0]);
+        $this->assertObjectHasAttribute('vector_color',       $response[0]);
+        $this->assertObjectHasAttribute('stroke_color',       $response[0]);
+        $this->assertObjectHasAttribute('select_color',       $response[0]);
+        $this->assertObjectHasAttribute('vector_opacity',     $response[0]);
+        $this->assertObjectHasAttribute('select_opacity',     $response[0]);
+        $this->assertObjectHasAttribute('stroke_opacity',     $response[0]);
+        $this->assertObjectHasAttribute('graphic_opacity',    $response[0]);
+        $this->assertObjectHasAttribute('stroke_width',       $response[0]);
+        $this->assertObjectHasAttribute('point_radius',       $response[0]);
+        $this->assertObjectHasAttribute('point_image',        $response[0]);
+        $this->assertObjectHasAttribute('map_focus',          $response[0]);
+        $this->assertObjectHasAttribute('coverage',           $response[0]);
+        $this->assertObjectHasAttribute('wmsAddress',         $response[0]);
+        $this->assertObjectHasAttribute('layers',             $response[0]);
 
     }
 
