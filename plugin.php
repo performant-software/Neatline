2f8aaf1a7b67fd9fa06853db938eaf9bb0ded9fb
David McClure <david.mcclure@virginia.edu>
2012-11-26 13:08:46 -0500

    Gets rid of DevController actions.

--- plugin.php
+++ plugin.php
@@ -21,7 +21,6 @@ if (!defined('NEATLINE_PLUGIN_DIR'))
 require_once NEATLINE_PLUGIN_DIR . '/NeatlinePlugin.php';
 require_once NEATLINE_PLUGIN_DIR . '/helpers/NeatlineFunctions.php';
 require_once NEATLINE_PLUGIN_DIR . '/forms/NeatlineDetails.php';
-require_once NEATLINE_PLUGIN_DIR . '/controllers/RestController.php';
 
 Zend_Registry::set('fileIn', 'php://input');
 
