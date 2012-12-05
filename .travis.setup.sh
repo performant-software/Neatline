347298a4ae3894febfa457a7b8fd1853e0dc0ac0
David McClure <david.mcclure@virginia.edu>
2012-12-05 11:42:55 -0500

    Adds grunt build step to travis runner.

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -32,5 +32,6 @@ sed -i 's/256M/512M/' $OMEKA_DIR/application/tests/bootstrap.php
 # symlink the plugin
 cd $OMEKA_DIR/plugins && ln -s $PLUGIN_DIR
 bundle install
+grunt build
 # export DISPLAY=:99.0
 # sh -e /etc/init.d/xvfb start
