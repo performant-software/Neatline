ec53683366491d76b4554533f4174edc72d75f21
David McClure <david.mcclure@virginia.edu>
2013-01-03 18:40:18 -0500

    Experimenting with collection support in Rivets config.

--- views/shared/javascripts/app/app.config.js
+++ views/shared/javascripts/app/app.config.js
@@ -16,19 +16,43 @@
  */
 rivets.configure({
   adapter: {
+
     subscribe: function(obj, keypath, callback) {
-      obj.on('change:' + keypath, callback)
+      if (obj instanceof Backbone.Collection) {
+        obj.on('add remove reset', function () {
+          callback(obj[keypath]);
+        });
+      } else {
+        obj.on('change:' + keypath, function (m, v) { callback(v) });
+      };
     },
+
     unsubscribe: function(obj, keypath, callback) {
-      obj.off('change:' + keypath, callback)
+      if (obj instanceof Backbone.Collection) {
+        obj.off('add remove reset', function () {
+          callback(obj[keypath]) 
+        });
+      } else {
+        obj.off('change:' + keypath, function (m, v) { callback(v) });
+      };
     },
+
     read: function(obj, keypath) {
-      return obj instanceof Backbone.Collection ?
-        obj["models"] : obj.get(keypath);
+      if (obj instanceof Backbone.Collection) {
+        return obj['models'];
+      } else {
+        return obj.get(keypath);
+      }
     },
+
     publish: function(obj, keypath, value) {
-      obj.set(keypath, value)
+      if (obj instanceof Backbone.Collection) {
+        obj[keypath] = value;
+      } else {
+        obj.set(keypath, value);
+      };
     }
+
   }
 });
 
