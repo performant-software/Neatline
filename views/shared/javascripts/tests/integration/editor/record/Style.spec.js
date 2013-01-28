682f7d4bec96472027e0893e5b8c93c4c52ad489
David McClure <david.mcclure@virginia.edu>
2013-01-27 21:07:05 -0500

    Sketches in test cases for form style previewing.

---
+++ views/shared/javascripts/tests/integration/editor/record/Style.spec.js
@@ -0,0 +1,45 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * Tests for record form style tab.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+describe('Record Form Style Tab', function() {
+
+
+  var els;
+
+
+  beforeEach(function() {
+
+    _t.loadEditor();
+    _t.openRecordForm();
+
+    els = {
+
+    };
+
+  });
+
+
+  it('should preview shape color');
+  it('should preview line color');
+  it('should preview selected color');
+  it('should preview shape opacity');
+  it('should preview selected opacity');
+  it('should preview line opacity');
+  it('should preview image opacity');
+  it('should preview line width');
+  it('should preview point radius');
+  it('should preview point radius');
+  it('should populate focus inputs');
+  it('should render current zoom');
+
+
+});
