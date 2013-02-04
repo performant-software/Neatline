9eeffd0898ada2f4426c336c3bb0ab93ffb424fa
David McClure <david.mcclure@virginia.edu>
2013-02-04 17:42:55 -0500

    Adds back in slug to front-end.

--- views/shared/exhibits/underscore/partials/_text_tab.php
+++ views/shared/exhibits/underscore/partials/_text_tab.php
@@ -17,22 +17,6 @@
 <div class="control-group">
 
   <?php echo $this->partial(
-    'exhibits/underscore/helpers/_text_input.php', array(
-      'name'  => 'item-id',
-      'label' => 'Item',
-      'bind'  => 'record.item_id'
-  )); ?>
-
-  <!--
-  <?php echo $this->partial(
-    'exhibits/underscore/helpers/_text_input.php', array(
-      'name'  => 'slug',
-      'label' => 'Slug',
-      'bind'  => 'record.slug'
-  )); ?>
-  -->
-
-  <?php echo $this->partial(
     'exhibits/underscore/helpers/_textarea.php', array(
       'name'  => 'title',
       'label' => 'Title',
@@ -44,6 +28,20 @@
       'name'  => 'body',
       'label' => 'Body',
       'bind'  => 'record.body'
+  )); ?>
+
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'item-id',
+      'label' => 'Item',
+      'bind'  => 'record.item_id'
+  )); ?>
+
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'slug',
+      'label' => 'Slug',
+      'bind'  => 'record.slug'
   )); ?>
 
 </div>
