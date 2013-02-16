83269ee606fa5932bacec25cc71bf5a9cdf3b243
David McClure <david.mcclure@virginia.edu>
2013-02-16 14:38:07 -0500

    Markup for default exhibit focus.

--- views/shared/exhibits/underscore/_exhibit.php
+++ views/shared/exhibits/underscore/_exhibit.php
@@ -15,6 +15,32 @@
 
 <script id="exhibit-template" type="text/templates">
 
+  <div class="control-group">
+
+    <?php echo $this->partial(
+      'exhibits/underscore/helpers/_text_input.php', array(
+        'name'  => 'map-focus',
+        'label' => 'Default Exhibit Focus'
+        // 'bind'  => 'record.map_focus'
+    )); ?>
+
+    <?php echo $this->partial(
+      'exhibits/underscore/helpers/_text_input.php', array(
+        'name'  => 'map-zoom',
+        'label' => 'Default Exhibit Zoom'
+        // 'bind'  => 'record.map_zoom'
+    )); ?>
+
+    <div class="controls">
+      <div class="inline-inputs">
+        <a name="set-focus" class="btn btn-small">
+          <i class="icon-map-marker"></i> Use Current Viewport as Default
+        </a>
+      </div>
+    </div>
+
+  </div>
+
   <a name="save" class="btn btn-large btn-primary" \>
     <i class="icon-ok icon-white"></i> <?php echo __('Save'); ?>
   </a>
