18d6ad4352018e5fa8e721e82eb941539d8d91c6
David McClure <david.mcclure@virginia.edu>
2013-02-16 16:52:10 -0500

    Standardizes vertical offset of form action buttons.

--- views/shared/exhibits/underscore/_exhibit.php
+++ views/shared/exhibits/underscore/_exhibit.php
@@ -15,8 +15,36 @@
 
 <script id="exhibit-template" type="text/templates">
 
-  <a name="save" class="btn btn-large btn-primary" \>
-    <i class="icon-ok icon-white"></i> <?php echo __('Save'); ?>
-  </a>
+  <div class="control-group">
+
+    <?php echo $this->partial(
+      'exhibits/underscore/helpers/_text_input.php', array(
+        'name'  => 'map-focus',
+        'label' => 'Default Map Focus'
+        // 'bind'  => 'record.map_focus'
+    )); ?>
+
+    <?php echo $this->partial(
+      'exhibits/underscore/helpers/_text_input.php', array(
+        'name'  => 'map-zoom',
+        'label' => 'Default Map Zoom'
+        // 'bind'  => 'record.map_zoom'
+    )); ?>
+
+    <div class="controls">
+      <div class="inline-inputs">
+        <a name="set-focus" class="btn btn-small">
+          <i class="icon-map-marker"></i> Use Current Viewport as Default
+        </a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="actions">
+    <a name="save" class="btn btn-large btn-primary" \>
+      <i class="icon-ok icon-white"></i> <?php echo __('Save'); ?>
+    </a>
+  </div>
 
 </script>
--- views/shared/exhibits/underscore/partials/_style_tab.php
+++ views/shared/exhibits/underscore/partials/_style_tab.php
@@ -14,138 +14,142 @@
 ?>
 
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'tags',
-    'label' => 'Tags',
-    'bind'  => 'record.tags'
-)); ?>
+<div class="control-group">
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_select.php', array(
-    'name'  => 'presenter',
-    'label' => 'Presenter',
-    'bind'  => 'record.presenter',
-    'options'   => _nl_getPresenters()
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'tags',
+      'label' => 'Tags',
+      'bind'  => 'record.tags'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'vector-color',
-    'label' => 'Shape Color',
-    'bind'  => 'record.vector_color',
-    'class' => 'preview',
-    'type'  => 'color'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_select.php', array(
+      'name'  => 'presenter',
+      'label' => 'Presenter',
+      'bind'  => 'record.presenter',
+      'options'   => _nl_getPresenters()
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'stroke-color',
-    'label' => 'Line Color',
-    'bind'  => 'record.stroke_color',
-    'class' => 'preview',
-    'type'  => 'color'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'vector-color',
+      'label' => 'Shape Color',
+      'bind'  => 'record.vector_color',
+      'class' => 'preview',
+      'type'  => 'color'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'select-color',
-    'label' => 'Selected Color',
-    'bind'  => 'record.select_color',
-    'class' => 'preview',
-    'type'  => 'color'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'stroke-color',
+      'label' => 'Line Color',
+      'bind'  => 'record.stroke_color',
+      'class' => 'preview',
+      'type'  => 'color'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'vector-opacity',
-    'label' => 'Shape Opacity',
-    'bind'  => 'record.vector_opacity',
-    'class' => 'preview opacity'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'select-color',
+      'label' => 'Selected Color',
+      'bind'  => 'record.select_color',
+      'class' => 'preview',
+      'type'  => 'color'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'stroke-opacity',
-    'label' => 'Line Opacity',
-    'bind'  => 'record.stroke_opacity',
-    'class' => 'preview opacity'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'vector-opacity',
+      'label' => 'Shape Opacity',
+      'bind'  => 'record.vector_opacity',
+      'class' => 'preview opacity'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'select-opacity',
-    'label' => 'Selected Opacity',
-    'bind'  => 'record.select_opacity',
-    'class' => 'preview opacity'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'stroke-opacity',
+      'label' => 'Line Opacity',
+      'bind'  => 'record.stroke_opacity',
+      'class' => 'preview opacity'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'image-opacity',
-    'label' => 'Image Opacity',
-    'bind'  => 'record.image_opacity',
-    'class' => 'preview opacity'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'select-opacity',
+      'label' => 'Selected Opacity',
+      'bind'  => 'record.select_opacity',
+      'class' => 'preview opacity'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'stroke-width',
-    'label' => 'Line Width',
-    'bind'  => 'record.stroke_width',
-    'class' => 'preview integer'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'image-opacity',
+      'label' => 'Image Opacity',
+      'bind'  => 'record.image_opacity',
+      'class' => 'preview opacity'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'point-radius',
-    'label' => 'Point Radius',
-    'bind'  => 'record.point_radius',
-    'class' => 'preview integer'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'stroke-width',
+      'label' => 'Line Width',
+      'bind'  => 'record.stroke_width',
+      'class' => 'preview integer'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'point-image',
-    'label' => 'Point Image',
-    'bind'  => 'record.point_image',
-    'class' => 'preview'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'point-radius',
+      'label' => 'Point Radius',
+      'bind'  => 'record.point_radius',
+      'class' => 'preview integer'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'min-zoom',
-    'label' => 'Min Zoom',
-    'bind'  => 'record.min_zoom',
-    'useCurrent' => true
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'point-image',
+      'label' => 'Point Image',
+      'bind'  => 'record.point_image',
+      'class' => 'preview'
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'max-zoom',
-    'label' => 'Max Zoom',
-    'bind'  => 'record.max_zoom',
-    'useCurrent' => true
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'min-zoom',
+      'label' => 'Min Zoom',
+      'bind'  => 'record.min_zoom',
+      'useCurrent' => true
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'map-focus',
-    'label' => 'Default Focus',
-    'bind'  => 'record.map_focus'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'max-zoom',
+      'label' => 'Max Zoom',
+      'bind'  => 'record.max_zoom',
+      'useCurrent' => true
+  )); ?>
 
-<?php echo $this->partial(
-  'exhibits/underscore/helpers/_text_input.php', array(
-    'name'  => 'map-zoom',
-    'label' => 'Default Zoom',
-    'bind'  => 'record.map_zoom'
-)); ?>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'map-focus',
+      'label' => 'Default Focus',
+      'bind'  => 'record.map_focus'
+  )); ?>
 
-<div class="controls">
-  <div class="inline-inputs">
-    <a name="set-focus" class="btn btn-small">
-      <i class="icon-map-marker"></i> Use Current Viewport as Default
-    </a>
+  <?php echo $this->partial(
+    'exhibits/underscore/helpers/_text_input.php', array(
+      'name'  => 'map-zoom',
+      'label' => 'Default Zoom',
+      'bind'  => 'record.map_zoom'
+  )); ?>
+
+  <div class="controls">
+    <div class="inline-inputs">
+      <a name="set-focus" class="btn btn-small">
+        <i class="icon-map-marker"></i> Use Current Viewport as Default
+      </a>
+    </div>
   </div>
+
 </div>
