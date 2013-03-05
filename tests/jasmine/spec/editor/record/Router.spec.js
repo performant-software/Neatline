585c9757476fb44886e099620aa3034373a5a024
David McClure <david.mcclure@virginia.edu>
2013-03-05 14:12:29 -0500

    Tests for tab-specific record route hydrations.

--- tests/jasmine/spec/editor/record/Router.spec.js
+++ tests/jasmine/spec/editor/record/Router.spec.js
@@ -13,20 +13,50 @@
 describe('Record Router', function() {
 
 
+  var href;
+
+
   beforeEach(function() {
     _t.loadEditor();
+    href = $(_t.getRecordRows()[0]).attr('href');
   });
 
 
   it('#record/:id', function() {
 
-    _t.navigate($(_t.getRecordRows()[1]).attr('href'));
+    _t.navigate(href);
 
-    // Record form should be visible.
+    // Record form should be visible, "Text" tab active.
     expect(_t.vw.EDITOR.__ui.editor).toContain(_t.vw.RECORD.$el);
-
-    // "Text" should be active.
     _t.assertActiveTab('text');
+
+  });
+
+
+  describe('#record/:id/:tab', function() {
+
+    afterEach(function() {
+      // Record form should be visible.
+      expect(_t.vw.EDITOR.__ui.editor).toContain(_t.vw.RECORD.$el);
+    });
+
+    it('text', function() {
+      _t.navigate(href+'/text');
+      // "Text" tab active.
+      _t.assertActiveTab('text');
+    });
+
+    it('spatial', function() {
+      _t.navigate(href+'/spatial');
+      // "Spatial" tab active.
+      _t.assertActiveTab('spatial');
+    });
+
+    it('style', function() {
+      _t.navigate(href+'/style');
+      // "Style" tab active.
+      _t.assertActiveTab('style');
+    });
 
   });
 
@@ -35,11 +65,37 @@ describe('Record Router', function() {
 
     _t.navigate('record/add');
 
-    // Record form should be visible.
+    // Record form should be visible, "Text" tab active.
     expect(_t.vw.EDITOR.__ui.editor).toContain(_t.vw.RECORD.$el);
-
-    // "Text" should be active.
     _t.assertActiveTab('text');
+
+  });
+
+
+  describe('#record/add/:tab', function() {
+
+    afterEach(function() {
+      // Record form should be visible.
+      expect(_t.vw.EDITOR.__ui.editor).toContain(_t.vw.RECORD.$el);
+    });
+
+    it('text', function() {
+      _t.navigate('record/add/text');
+      // "Text" tab active.
+      _t.assertActiveTab('text');
+    });
+
+    it('spatial', function() {
+      _t.navigate('record/add/spatial');
+      // "Spatial" tab active.
+      _t.assertActiveTab('spatial');
+    });
+
+    it('style', function() {
+      _t.navigate('record/add/style');
+      // "Style" tab active.
+      _t.assertActiveTab('style');
+    });
 
   });
 
