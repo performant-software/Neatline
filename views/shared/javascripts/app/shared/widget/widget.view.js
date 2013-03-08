e5bf8079fe0cb81c15ae3797d7e1d278e6b6cf38
David McClure <david.mcclure@virginia.edu>
2013-03-07 19:30:26 -0500

    Implements abstract widget view.

--- views/shared/javascripts/app/shared/widget/widget.view.js
+++ views/shared/javascripts/app/shared/widget/widget.view.js
@@ -2,9 +2,7 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
 
 /**
- * Abstract view for plugin widgets. On startup, check to see if the view
- * element is attached to the DOM (which is the case if the view's `id` is
- * directly templated on the page). If not, append it to `#neatline`.
+ * Abstract view for plugin widgets.
  *
  * @package     omeka
  * @subpackage  neatline
@@ -17,7 +15,24 @@ Neatline.module('Shared.Widget', function(
 
 
   Widget.View = Backbone.Neatline.View.extend({
-    // TODO
+
+
+    className: 'widget',
+
+
+    /**
+     * On startup, check to see if the view element is attached to the DOM
+     * (which is the case if an element with the view's `id` is directly
+     * templated on the page). If not, append the view element to the core
+     * `#neatline` container.
+     */
+    init: function() {
+      if (!$.contains(document.documentElement, this.el)) {
+        this.$el.appendTo($('#neatline'));
+      }
+    },
+
+
   });
 
 
