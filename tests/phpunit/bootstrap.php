1fe6961ef073ab73824f4cbafd6814fda7399736
David McClure <david.mcclure@virginia.edu>
2013-03-18 17:51:59 -0400

    More cleanup in bootstrap.php.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -15,11 +15,11 @@
 define('NL_DIR', dirname(dirname(dirname(__FILE__))));
 define('OMEKA_DIR', dirname(dirname(NL_DIR)));
 
-// Omeka, Neatline.
+// Bootstrap Omeka, load Neatline plugin.
 require_once OMEKA_DIR . '/application/tests/bootstrap.php';
 require_once NL_DIR . '/NeatlinePlugin.php';
 
-// Helpers.
+// Load abstract test cases, mock filters.
 require_once 'Neatline_AbstractTestCase.php';
 require_once 'Neatline_TestCase.php';
 require_once 'mocks/filters.php';
