c219b2d60d250398c821c1cb3d84a2df80f4b8e4
David McClure <david.mcclure@virginia.edu>
2013-03-25 14:28:26 -0400

    Tests for keywords and range import queries.

--- tests/phpunit/unit/Jobs/ItemImporterTest.php
+++ tests/phpunit/unit/Jobs/ItemImporterTest.php
@@ -16,10 +16,79 @@ class ItemImporterTest_Explode extends Neatline_TestCase
 
 
     /**
-     * `ItemImporter` should create Neatline records for all Omeka items
-     * that match the search query.
+     * `ItemImporter` should match keyword queries.
      */
-    public function testCreateRecords()
+    public function testMatchKeywords()
+    {
+
+        $item1 = $this->__item('David McClure');
+        $item2 = $this->__item('David William McClure');
+        $item3 = $this->__item('Kara Weisman');
+        $exhibit = $this->__exhibit();
+
+        Zend_Registry::get('bootstrap')->getResource('jobs')->
+            send('ItemImporter', array(
+                'query' => array('search' => 'David'),
+                'exhibit_id' => $exhibit->id
+            )
+        );
+
+        // Should match items 1 and 2.
+        $records = $this->__records->queryRecords($exhibit);
+        $this->assertEquals($records['records'][0]['item_id'],$item1->id);
+        $this->assertEquals($records['records'][1]['item_id'],$item2->id);
+        $this->assertEquals($records['count'], 2);
+
+    }
+
+
+    /**
+     * `ItemImporter` should match range queries.
+     */
+    public function testMatchRange()
+    {
+
+        $item1 = $this->__item();
+        $item2 = $this->__item();
+        $exhibit = $this->__exhibit();
+
+        Zend_Registry::get('bootstrap')->getResource('jobs')->
+            send('ItemImporter', array(
+                'query' => array('range' => $item1->id),
+                'exhibit_id' => $exhibit->id
+            )
+        );
+
+        // Should match item 1.
+        $records = $this->__records->queryRecords($exhibit);
+        $this->assertEquals($records['records'][0]['item_id'],$item1->id);
+        $this->assertEquals($records['count'], 1);
+
+    }
+
+
+    /**
+     * `ItemImporter` should match collection queries.
+     */
+    public function testMatchCollection()
+    {
+        // TODO
+    }
+
+
+    /**
+     * `ItemImporter` should match type queries.
+     */
+    public function testMatchType()
+    {
+        // TODO
+    }
+
+
+    /**
+     * `ItemImporter` should match tags queries.
+     */
+    public function testMatchTags()
     {
         // TODO
     }
