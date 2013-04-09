261d37755561db81f26cb84c365477cf324d4f3c
David McClure <david.mcclure@virginia.edu>
2013-04-09 18:19:36 -0400

    Ready for revamped fixtures approach.

--- tests/phpunit/fixtures/JsonTest.php
+++ tests/phpunit/fixtures/JsonTest.php
@@ -56,6 +56,115 @@ class FixturesTest_Json extends Neatline_TestCase
 
     /**
      * GET /records
+     * `records.standard.json`
+     * `records.changed.json`
+     * `records.removed.json`
+     */
+    public function testRecordsJson()
+    {
+
+        $record1 = $this->__record($this->exhibit);
+        $record2 = $this->__record($this->exhibit);
+        $record3 = $this->__record($this->exhibit);
+
+        // `records.standard.json`
+        // - #1 has a default map focus/zoom, #2 and #3 do not.
+
+        $record1->item_id           = 1;
+        $record1->title             = 'title1';
+        $record1->body              = 'body1';
+        $record1->tags              = 'tags1';
+        $record1->coverage          = 'POINT(1 2)';
+        $record1->widgets           = 'Widget1,Widget3';
+        $record1->presenter         = 'Presenter2';
+        $record1->fill_color        = '#111111';
+        $record1->select_color      = '#222222';
+        $record1->stroke_color      = '#333333';
+        $record1->fill_opacity      = 4;
+        $record1->select_opacity    = 5;
+        $record1->stroke_opacity    = 6;
+        $record1->stroke_width      = 7;
+        $record1->point_radius      = 8;
+        $record1->weight            = 9;
+        $record1->start_date        = '10';
+        $record1->end_date          = '11';
+        $record1->point_image       = '12';
+        $record1->wms_address       = '13';
+        $record1->wms_layers        = '14';
+        $record1->min_zoom          = 15;
+        $record1->max_zoom          = 16;
+        $record1->show_after_date   = '17';
+        $record1->show_before_date  = '18';
+        $record1->map_focus         = '100,200';
+        $record1->map_zoom          = 10;
+
+        $record2->title             = 'title2';
+        $record3->title             = 'title3';
+        $record2->body              = 'body2';
+        $record3->body              = 'body3';
+        $record2->coverage          = 'POINT(3 4)';
+        $record3->coverage          = 'POINT(5 6)';
+
+        $record1->__save();
+        $record2->__save();
+        $record3->__save();
+
+        $this->writeFixtureFromRoute('neatline/records',
+            'records.standard.json');
+
+    }
+
+
+    /**
+     * GET /records
+     * `records.standard.json`
+     */
+    // public function testRecordsJson()
+    // {
+
+    //     $record1 = $this->__record($this->exhibit);
+    //     $record2 = $this->__record($this->exhibit);
+    //     $record3 = $this->__record($this->exhibit);
+
+    //     $record1->setArray(array(
+    //         'item_id'           => 1,
+    //         'title'             => 'Title1',
+    //         'body'              => 'Body1',
+    //         'coverage'          => 'POINT(1 1)',
+    //         'tags'              => 'tags1',
+    //         'widgets'           => 'Widget1',
+    //         'presenter'         => 'Presenter1',
+    //         'fill_color'        => '#111111',
+    //         'select_color'      => '#222222',
+    //         'stroke_color'      => '#333333',
+    //         'point_image'       => 'image1',
+    //         'fill_opacity'      => 1,
+    //         'select_opacity'    => 2,
+    //         'stroke_opacity'    => 3,
+    //         'stroke_width'      => 4,
+    //         'point_radius'      => 5,
+    //         'min_zoom'          => 6,
+    //         'max_zoom'          => 7,
+    //         'map_zoom'          => 8,
+    //         'map_focus'         => '1,1',
+    //         'wms_address'       => 'address1',
+    //         'wms_layers'        => 'layers1',
+    //         'start_date'        => '2001',
+    //         'end_date'          => '2002',
+    //     ));
+
+    //     $record1->__save();
+    //     $record2->__save();
+    //     $record3->__save();
+
+    //     $this->writeFixtureFromRoute('neatline/records',
+    //         'records.standard.json');
+
+    // }
+
+
+    /**
+     * GET /records
      * `records.vector.standard.json`
      * `records.vector.changed.json`
      */
