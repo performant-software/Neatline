10ef654091c8ef133ff83b061b774e7b270cfbc9
David McClure <david.mcclure@virginia.edu>
2013-04-11 17:58:16 -0400

    Stubs for map layer refresh tests.

---
+++ tests/jasmine/suites/public/map/LayerRefreshing.spec.js
@@ -0,0 +1,25 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * Tests for map layer refreshing.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+describe('Map Layer Refreshing', function() {
+
+
+  beforeEach(function() {
+    _t.loadNeatline();
+  });
+
+
+  it('should clear and rebuild all layers');
+  it('should not rebuild frozen layers');
+
+
+});
