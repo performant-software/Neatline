58a34c38bbf7963f8521c02804fb5f7b5cc7a276
David McClure <david.mcclure@virginia.edu>
2013-04-18 19:43:26 -0400

    Gets rid of extraneous docblock comments in Jasmine suite.

--- tests/jasmine/suites/editor/records/Router.spec.js
+++ tests/jasmine/suites/editor/records/Router.spec.js
@@ -2,8 +2,6 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
 
 /**
- * Records router test.
- *
  * @package     omeka
  * @subpackage  neatline
  * @copyright   2012 Rector and Board of Visitors, University of Virginia
--- tests/jasmine/suites/editor/search/Query.spec.js
+++ tests/jasmine/suites/editor/search/Query.spec.js
@@ -2,8 +2,6 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
 
 /**
- * Search execution tests.
- *
  * @package     omeka
  * @subpackage  neatline
  * @copyright   2012 Rector and Board of Visitors, University of Virginia
