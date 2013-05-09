f3a4ddb74666143ad2581a37fc11ee8fff1d440b
David McClure <david.mcclure@virginia.edu>
2013-05-09 12:33:12 -0400

    Migrates all search suites to new fixtures system.

---
+++ tests/phpunit/fixtures/EditorSearchQueryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class FixturesTest_EditorSearchQuery extends Neatline_FixtureCase
+{
+
+
+    /**
+     * `EditorSearchQuery.records.json`
+     */
+    public function testSearchQuery()
+    {
+
+        $record1 = $this->__record($this->exhibit);
+        $record2 = $this->__record($this->exhibit);
+        $record3 = $this->__record($this->exhibit);
+
+        $record1->title = 'result1';
+        $record2->title = 'result2';
+        $record3->title = 'result3';
+        $record1->added = '2003-01-01';
+        $record2->added = '2002-01-01';
+        $record3->added = '2001-01-01';
+
+        $record1->save();
+        $record2->save();
+        $record3->save();
+
+        $this->writeFixtureFromRoute('neatline/records',
+            'EditorSearchQuery.records.json'
+        );
+
+    }
+
+
+}
