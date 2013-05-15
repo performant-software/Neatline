48c6f5d32e6b46e5d43f1ce4a47e20c22e01e3f7
David McClure <david.mcclure@virginia.edu>
2013-05-15 16:13:04 -0400

    Ready for item import fix.

--- .dev/insert.js
+++ .dev/insert.js
@@ -1,21 +1,28 @@
-/*
- * Create exhibit.
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
+
 
 var mysql = require('mysql');
 var randy = require('randy');
-var _s = require('underscore.string');
-var _ = require('underscore');
-var fs = require('fs');
+var _s    = require('underscore.string');
+var _     = require('underscore');
+var fs    = require('fs');
 
 
 // Connect to MySQL.
 var client = mysql.createConnection({
-  host: 'localhost',
-  user: 'omeka',
+  host:     'localhost',
+  user:     'omeka',
   password: 'omeka',
-  port: 8889,
-  database: 'tei'
+  port:     8889,
+  database: 'alpha2'
 });
 
 client.connect();
@@ -26,7 +33,7 @@ function color() {
 }
 
 
-function records(exhibit_id, count, zoom, color, tags) {
+function records(exhibit_id, count, zoom) {
 
   // Base insert.
   var sql = 'INSERT INTO omeka_neatline_records (' +
@@ -54,32 +61,35 @@ function records(exhibit_id, count, zoom, color, tags) {
   _(count).times(function(n) {
 
     var off = randy.randInt(0,len-500);
-    var lat = randy.randInt(-20000000,20000000);
-    var lon = randy.randInt(-20000000,20000000);
+    var lon = randy.randInt(-14277213,-7301264);
+    var lat = randy.randInt(2726050,6522218);
     var geo = 'GeomFromText("POINT('+lon+' '+lat+')")';
     var rad = randy.randInt(10,100);
     var bod = wp.slice(off, off+500);
 
+    var tags = 'precinct, '+
+      randy.choice(['democrat', 'republican']);
+
     sql += '(' +
-      exhibit_id+','+
-      1+','+
-      '"Record'+n+'",'+
-      '"'+bod+'",'+
-      '"'+tags+'",'+
-      '"StaticBubble",'+
-      '"'+color+'",'+
-      '"'+color+'",'+
-      '"#000000",'+
-      '"#000000",'+
-      0.3+','+
-      0.5+','+
-      0.8+','+
-      1.0
-      rad+','+
-      1+','+
-      zoom+','+
-      zoom+','+
-      geo+
+      exhibit_id+','+       // exhibit_id
+      1+','+                // is_coverage
+      '"Precinct '+n+'",'+  // title
+      '"'+bod+'",'+         // body
+      '"'+tags+'",'+        // tags
+      '"StaticBubble",'+    // presenter
+      '"#000000",'+         // fill_color
+      '"#000000",'+         // fill_color_select
+      '"#000000",'+         // stroke_color
+      '"#000000",'+         // stroke_color_select
+      0.5+','+              // fill_opacity
+      0.6+','+              // fill_opacity_select
+      0.8+','+              // stroke_opacity
+      1.0+','+              // stroke_opacity_select
+      rad+','+              // point_radius
+      0+','+                // stroke_width
+      zoom+','+             // min_zoom
+      zoom+','+             // max_zoom
+      geo+                  // coverage
     ')';
 
     if (n != count-1) sql += ',';
@@ -95,7 +105,7 @@ var wp, len;
 
 // Create exhibit.
 var sql = 'INSERT INTO omeka_neatline_exhibits ' +
-  '(title, slug, base_layer) VALUES ("dev", "dev", "OpenStreetMap")';
+  '(title, slug, base_layer) VALUES ("Neatline Stylesheets", "neatline-stylesheets", "OpenStreetMap")';
 
 client.query(sql, function(err, res) {
 
@@ -105,12 +115,11 @@ client.query(sql, function(err, res) {
     wp = text;
     len = text.length
 
-    records(res.insertId, 200, 3, color(), 'level3');
-    records(res.insertId, 400, 4, color(), 'level4');
-    records(res.insertId, 1000, 5, color(), 'level5');
-    records(res.insertId, 20000, 6, color(), 'level6');
-    records(res.insertId, 50000, 7, color(), 'level7');
-    records(res.insertId, 100000, 8, color(), 'level8');
+    records(res.insertId, 200, 6, color());
+    records(res.insertId, 1000, 7, color());
+    records(res.insertId, 5000, 8, color());
+    records(res.insertId, 20000, 9, color());
+    records(res.insertId, 73800, 10, color());
 
     client.end(function() {
       process.exit();
--- .dev/truncate.js
+++ .dev/truncate.js
@@ -1,6 +1,13 @@
-/*
- * Create exhibit.
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
+
 
 var mysql = require('mysql');
 
@@ -9,7 +16,7 @@ var client = mysql.createConnection({
   user: 'omeka',
   password: 'omeka',
   port: 8889,
-  database: 'tei'
+  database: 'alpha2'
 });
 
 client.connect();
