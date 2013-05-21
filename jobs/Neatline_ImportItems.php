b979d055599bc9b27430c50372f88186dc978b6e
David McClure <david.mcclure@virginia.edu>
2013-05-21 16:13:14 -0400

    Sketches in query set paging in Neatline_ImportItems.

--- jobs/Neatline_ImportItems.php
+++ jobs/Neatline_ImportItems.php
@@ -20,36 +20,50 @@ class Neatline_ImportItems extends Omeka_Job_AbstractJob
     public function perform()
     {
 
+        // Since this code isn't running in the context of a regular web
+        // request, the hierarchy of view template directories hasn't been
+        // set up by the controller. In order for the item compilation to
+        // work properly, we need to create new instance of `Omeka_View`,
+        // set it in the registry, and manually register the directories
+        // where the view should search for template files - first in the
+        // theme, then in the Neatline plugin, then in the Omeka core.
+
         nl_mockView();
 
         // Manually set the base web directory passed from the controller
         // action. This ensures that links to file attachments will point
         // to the web-accessible locations, not to the local filesystem.
+
         nl_setWebDir($this->_options['web_dir']);
 
-        // Get application tables.
-        $__exhibits = $this->_db->getTable('NeatlineExhibit');
         $__records  = $this->_db->getTable('NeatlineRecord');
+        $__exhibits = $this->_db->getTable('NeatlineExhibit');
+        $__items    = $this->_db->getTable('Item');
 
-        // Load exhibit, query items.
+        // Load the exhibit, alias the query.
         $exhibit = $__exhibits->find($this->_options['exhibit_id']);
-        $items = get_records('Item', $this->_options['query'], 100000);
+        $query = $this->_options['query'];
 
-        foreach ($items as $item) {
+        $i = 0;
+        while ($items = $__items->findBy($query, 10, $i)) {
+            foreach ($items as $item) {
 
-            // Try to find a record.
-            $record = $__records->findBySql('exhibit_id=? && item_id=?',
-                array($exhibit->id, $item->id), true
-            );
+                // Try to find an existing record.
+                $record = $__records->findBySql(
+                    'exhibit_id=? && item_id=?',
+                    array($exhibit->id, $item->id), true
+                );
 
-            // Otherwise, create one.
-            if (!$record) {
-                $record = new NeatlineRecord($exhibit, $item);
-                $record->added = $item->added;
+                // Otherwise, create one.
+                if (!$record) {
+                    $record = new NeatlineRecord($exhibit, $item);
+                    $record->added = $item->added;
+                }
+
+                $record->save();
+
             }
-
-            $record->save();
-
+            $i++;
         }
 
     }
