8afb7f162483bc6f550e2d103a14e1da4b75081d
David McClure <david.mcclure@virginia.edu>
2013-06-02 17:16:21 -0400

    Renames Svg Jasmine suite to SVG.

--- tests/jasmine/specs/editor/integration/record/Edit.spec.js
+++ tests/jasmine/specs/editor/integration/record/Edit.spec.js
@@ -24,7 +24,7 @@ describe('Record Form Edit', function() {
   it('should update edit layer model', function() {
 
     // --------------------------------------------------------------------
-    // When the record form model changes, the `nModel` `attribute on the
+    // When the record form model changes, the `nModel` attribute on the
     // map edit layer should be updated.
     // --------------------------------------------------------------------
 
@@ -35,38 +35,6 @@ describe('Record Form Edit', function() {
 
     // Edit layer `nModel` should be updated.
     expect(NL.vw.MAP.editLayer.nModel.id).toEqual(999);
-
-  });
-
-
-  it('should update edit layer in id-to-layer tracker', function() {
-
-    // --------------------------------------------------------------------
-    // When a new record is saved for the first time and the `id` field on
-    // the record form model is populated for the first time, the tracker
-    // object that associates id's with vector layers should be updated so
-    // that the new id points to the edit layer. This prevents the edit
-    // layer from being "double-loaded" - once as the existing edit layer
-    // that was created with the new record was added (identified by the
-    // `undefined` key in the tracker), and again as the newly-created
-    // record on the server that will start getting loaded when the map is
-    // moved (identified by the new id). By replacing the `undefined` key
-    // with the new id, we block the new record from being loaded, since
-    // the record is already represented on the map by the edit layer.
-    // --------------------------------------------------------------------
-
-    NL.navigate('record/add');
-
-    // Update record form.
-    NL.vw.RECORD.model.set('id', 999);
-
-    // `undefined` key should be replaced with new id.
-    expect(NL.vw.MAP.layers.vector[undefined]).toBeUndefined();
-
-    // The new id should point to the edit layer.
-    expect(NL.vw.MAP.layers.vector[999].id).toEqual(
-      NL.vw.MAP.editLayer.id
-    );
 
   });
 
