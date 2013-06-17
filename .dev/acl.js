31c9eb0affba615ed7c92d1c4aacfefce0465222
David McClure <david.mcclure@virginia.edu>
2013-06-17 18:00:26 -0400

    Scrubs out .dev directory.

--- .dev/acl.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-
-request({
-  url: 'http://localhost:8888/alpha2/neatline/records/debug'
-  // method: 'POST',
-  // body: JSON.stringify({exhibit_id : 4, title: 'test'})
-}, function(err, response, body) {
-  console.log(response.body);
-});
