521ef1ed733b5d0bdc6f5fc6100dc63f865c0ff4
David McClure <david.mcclure@virginia.edu>
2013-07-19 10:35:47 -0500

    Gets rid of migration helpers.

--- migrations/abstract/Neatline_Migration_Abstract.php
+++ migrations/abstract/Neatline_Migration_Abstract.php
@@ -41,39 +41,4 @@ abstract class Neatline_Migration_Abstract
     }
 
 
-    /**
-     * Change an existing column's name.
-     *
-     * @param string $table The name of the table.
-     * @param string $oldName The name of the column to be changed.
-     * @param string $newName The new name for the column.
-     * @param string $type The new type for the column
-     */
-    private function _changeColumn($table, $oldName, $newName, $type)
-    {
-        $sql = <<<SQL
-        ALTER TABLE {$this->db->prefix}$table
-        CHANGE COLUMN $oldName $newName $type;
-SQL;
-        $this->db->query($sql);
-    }
-
-
-    /**
-     * Add a new column.
-     *
-     * @param string $table The name of the table.
-     * @param string $name The new column name.
-     * @param string $type The new column type.
-     */
-    private function _addColumn($table, $name, $type)
-    {
-        $sql = <<<SQL
-        ALTER TABLE {$this->db->prefix}$table
-        ADD COLUMN $name $type;
-SQL;
-        $this->db->query($sql);
-    }
-
-
 }
