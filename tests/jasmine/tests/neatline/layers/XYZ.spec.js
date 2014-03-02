86b4498a4a76f4db781da7241dec705f3960cf8f
David McClure <david.mcclure@virginia.edu>
2014-03-02 12:12:18 -0800

    Adds XYZ layer tests.

---
+++ tests/jasmine/tests/neatline/layers/XYZ.spec.js
@@ -0,0 +1,41 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+describe('Layers | XYZ', function() {
+
+
+  beforeEach(function() {
+    NL.loadNeatline();
+  });
+
+
+  it('should construct a WMS layer', function() {
+
+    var xyz = Neatline.request('MAP:LAYERS:XYZ', {
+      title: 'Title',
+      properties: {
+        urls: [
+          'url1',
+          'url2',
+          'url3',
+          'url4'
+        ]
+      }
+    });
+
+    expect(xyz.CLASS_NAME).toEqual('OpenLayers.Layer.XYZ');
+    expect(xyz.name).toEqual('Title');
+
+    expect(xyz.url).toEqual(['url1', 'url2', 'url3', 'url4']);
+
+  });
+
+
+});
