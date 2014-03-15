38a474129a9c2476b1027185110614b271c7926a
David McClure <david.mcclure@virginia.edu>
2014-03-14 18:11:42 -0700

    When getting the OpenLayers theme directory, just pass the relative path to url(), instead of deriving it from an asset path.

--- helpers/Assets.php
+++ helpers/Assets.php
@@ -151,5 +151,5 @@ function nl_getPublicThemeDir()
  */
 function nl_getOpenLayersThemeDir()
 {
-    return dirname(img('dark/slider.png')).'/';
+    return url('plugins/Neatline/views/shared/images/dark/');
 }
