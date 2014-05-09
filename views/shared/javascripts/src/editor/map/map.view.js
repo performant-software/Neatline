bb316290cfabdb142dcf7105509ff20cb6e8c675
David McClure <davidwilliammcclure@gmail.com>
2014-05-08 20:16:28 -0700

    Bumps copyright to 2014, because time.

--- views/shared/javascripts/src/editor/map/map.view.js
+++ views/shared/javascripts/src/editor/map/map.view.js
@@ -4,7 +4,7 @@
  *
  * @package     omeka
  * @subpackage  neatline
- * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @copyright   2014 Rector and Board of Visitors, University of Virginia
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
 
