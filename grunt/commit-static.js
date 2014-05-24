d6ac6993d6e137e621ed46a5cec5c28b990719a8
David McClure <davidwilliammcclure@gmail.com>
2014-05-24 15:07:18 -0700

    Sketches in a commit-static Grunt task.

--- grunt/commit-static.js
+++ grunt/commit-static.js
@@ -6,13 +6,38 @@
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
 
+var sh = require('execSync');
+var glob = require('glob');
+var path = require('path');
+
 module.exports = function(grunt) {
 
   grunt.registerTask(
     'commit-static',
     'Commit minified payloads.',
     function() {
-      console.log('commit-static');
+
+      // Get the starting HEAD.
+      var head = sh.exec('git rev-prase HEAD');
+
+      // Glob the minified payloads.
+      var dist = path.resolve(__dirname, '../views/**/dist/**');
+
+      // Commit the payloads.
+      sh.exec('git add -f ' + dist);
+      sh.exec('git commit -am "Committing payloads."');
+
+      // Untrack the payloads.
+      sh.exec('git rm -r ' + dist);
+      sh.exec('git commit -am "Untracking payloads."');
+
+      // Reset to original HEAD.
+      sh.exec('git reset --soft ' + head);
+
+      // Commit both changes in a single commit.
+      sh.exec('git add -f ' + dist);
+      sh.exec('git commit -am "Committing payloads."');
+
     });
 
 };
