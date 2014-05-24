86a394103a2dffc8b2e70a644ac2a1e576a90018
David McClure <davidwilliammcclure@gmail.com>
2014-05-24 12:41:52 -0700

    Moves the task aliases out of the Gruntfile and into grunt/aliases.yaml.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,6 @@
 module.exports = function(grunt) {
 
 
-  // CONFIGURATION
-  // --------------------------------------------------------------------------
-
-
   // Read configuration files:
   var paths = grunt.file.readJSON('paths.json');
   var pkg = grunt.file.readJSON('package.json');
@@ -26,62 +22,6 @@ module.exports = function(grunt) {
       pkg: pkg
     }
   });
-
-
-  // TASKS
-  // --------------------------------------------------------------------------
-
-
-  // Run the tests by default.
-  grunt.registerTask('default', 'test');
-
-  // Build the application.
-  grunt.registerTask('build', [
-    'clean',
-    'bower',
-    'compile:min',
-    'copy'
-  ]);
-
-  // Concat static assets.
-  grunt.registerTask('compile', [
-    'stylus',
-    'concat'
-  ]);
-
-  // Minify static assets.
-  grunt.registerTask('compile:min', [
-    'compile',
-    'uglify',
-    'cssmin'
-  ]);
-
-  // Mount public Jasmine suite.
-  grunt.registerTask('jasmine:neatline:browser', [
-    'jasmine:neatline:build',
-    'connect'
-  ]);
-
-  // Mount editor Jasmine suite.
-  grunt.registerTask('jasmine:editor:browser', [
-    'jasmine:editor:build',
-    'connect'
-  ]);
-
-  // Run all tests.
-  grunt.registerTask('test', [
-    'compile:min',
-    'clean:fixtures',
-    'phpunit',
-    'jasmine'
-  ]);
-
-  // Spawn release package.
-  grunt.registerTask('package', [
-    'clean:dist',
-    'compile:min',
-    'compress'
-  ]);
 
 
 };
