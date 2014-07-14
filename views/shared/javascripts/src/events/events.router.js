63a706cb7f1fe22a046a978c77d03db16880a1d7
David McClure <davidwilliammcclure@gmail.com>
2014-07-14 08:31:50 -0700

    Public router working, breaking editor record/:id routes.

---
+++ views/shared/javascripts/src/events/events.router.js
@@ -0,0 +1,53 @@
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2014 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+Neatline.module('Events', function(Events) {
+
+
+  Events.Router = Neatline.Shared.Router.extend({
+
+
+    slug: 'EVENTS',
+
+    routes: {
+      'record/:id': 'record/:id'
+    },
+
+
+    /**
+     * Create a shell collection to fetch records.
+     */
+    initialize: function() {
+      this.records = new Neatline.Shared.Record.Collection();
+    },
+
+
+    /**
+     * Select a record.
+     *
+     * @param {String} id: The record id.
+     */
+    'record/:id': function(id) {
+
+      // Load the model.
+      this.records.getOrFetch(id, function(model) {
+
+        // Select the requested record.
+        Neatline.vent.trigger('select', {
+          model: model, source: this.slug
+        });
+
+      });
+
+    }
+
+
+  });
+
+
+});
