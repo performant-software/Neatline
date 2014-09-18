09ff0ebd1cfd2a945f40eae255983fbeda4f04c2
csbailey5t <csb5t@virginia.edu>
2014-09-18 15:40:44 -0400

    Adds Jasmine and PHPUnit tests for WMS layer selected opacity feature

--- tests/jasmine/tests/neatline/map/RenderWmsOpacity.spec.js
+++ tests/jasmine/tests/neatline/map/RenderWmsOpacity.spec.js
@@ -16,19 +16,92 @@ describe('Map | Render WMS Opacity', function() {
 
   beforeEach(function() {
     NL.loadNeatline();
+    NL.respondMap200(fixtures.records);
+
   });
 
 
-  it('should render opacities', function() {
+  it('should render the starting opacity', function() {
 
     // ------------------------------------------------------------------------
     // WMS layer opacities should be set from the `fill_opacity` field.
     // ------------------------------------------------------------------------
 
-    NL.respondMap200(fixtures.records);
-
     // Should set WMS layer opacity.
     expect(NL.v.map.getWmsLayers()[0].opacity).toEqual(0.5);
+
+  });
+
+  it('should bump opacity on highlight', function() {
+
+    // ------------------------------------------------------------------------
+    // WMS layer opacities should be set from the `fill_opacity_select` field.
+    // ------------------------------------------------------------------------
+
+    var layer = NL.v.map.getWmsLayers()[0];
+    Neatline.vent.trigger('highlight', { model: layer.neatline.model });
+
+    // Should apply the selected opacity to the WMS layer.
+    expect(layer.opacity).toEqual(0.8);
+
+  });
+
+    it('should lower opacity on unhighlight', function() {
+
+    // ------------------------------------------------------------------------
+    // WMS layer opacities should be set from the `fill_opacity_select` field.
+    // ------------------------------------------------------------------------
+
+    var layer = NL.v.map.getWmsLayers()[0];
+    Neatline.vent.trigger('highlight', { model: layer.neatline.model });
+    Neatline.vent.trigger('unhighlight', { model: layer.neatline.model });
+
+    // Should lower opacity to the default for the WMS layer.
+    expect(layer.opacity).toEqual(0.5);
+
+  });
+
+  it('should bump opacity on select', function() {
+
+    // ------------------------------------------------------------------------
+    // WMS layer opacities should be set from the `fill_opacity_select` field.
+    // ------------------------------------------------------------------------
+
+    var layer = NL.v.map.getWmsLayers()[0];
+    Neatline.vent.trigger('select', { model: layer.neatline.model });
+
+    // Should apply the selected opacity to the WMS layer.
+    expect(layer.opacity).toEqual(0.8);
+
+  });
+
+  it('should lower opacity on unselect', function() {
+
+    // ------------------------------------------------------------------------
+    // WMS layer opacities should be set from the `fill_opacity_select` field.
+    // ------------------------------------------------------------------------
+
+    var layer = NL.v.map.getWmsLayers()[0];
+    Neatline.vent.trigger('select', { model: layer.neatline.model });
+    Neatline.vent.trigger('unselect', { model: layer.neatline.model });
+
+    // Should lower opacity to the default for the WMS layer.
+    expect(layer.opacity).toEqual(0.5);
+
+  });
+
+  it('should not unhighlight a selected record', function() {
+
+    // ------------------------------------------------------------------------
+    // WMS layer opacities should be set from the `fill_opacity_select` field.
+    // ------------------------------------------------------------------------
+
+    var layer = NL.v.map.getWmsLayers()[0];
+    Neatline.vent.trigger('select', { model: layer.neatline.model });
+    Neatline.vent.trigger('unhighlight', { model: layer.neatline.model });
+
+    // Should lower opacity to the default for the WMS layer.
+    expect(layer.opacity).toEqual(0.8);
 
   });
 
