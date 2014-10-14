e96cec84807a02deffcd04ba1fb845cf340e6902
David McClure <davidwilliammcclure@gmail.com>
2014-10-14 16:09:08 -0700

    Gets widget injection tests passing.

--- views/shared/javascripts/src/shared/widget/widget.view.js
+++ views/shared/javascripts/src/shared/widget/widget.view.js
@@ -16,14 +16,40 @@ Neatline.module('Shared.Widget', function(Widget) {
 
 
     /**
-     * TODO|bandaid
-     * Inject the widget container into the map.
+     * Try to attach the view to an existing element, identified by the `id`
+     * attribute on the class definition.
+     *
+     * @param {Object} options
+     */
+    constructor: function(options) {
+
+      options = options || {};
+
+      // Attach the view to existing element.
+      var el = $('#'+this.id);
+      if (el.length) options.el = el;
+
+      Widget.View.__super__.constructor.call(this, options);
+
+    },
+
+
+    /**
+     * Ensure that the container element is appended to the DOM.
      *
      * @param {Object} options
      */
     initialize: function(options) {
-      this.$el.appendTo($('#neatline-map'));
+
+      options = options || {};
+
+      // Inject the container, if it isn't already in the DOM.
+      if (!$.contains(document, this.el)) {
+        this.$el.appendTo($('#neatline-map'));
+      }
+
       Widget.View.__super__.initialize.call(this, options);
+
     },
 
 
