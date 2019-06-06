performant-software/Neatline

commit 9a3021481f5bf02281266345c88403a66d28e13a

Escapes value for `extent` parameter.
Bumps version to 2.6.2.

models/NeatlineRecordTable.php
@@ -163,8 +163,8 @@ class NeatlineRecordTable extends Neatline_Table_Expandable
             $bb = $this->params['extent'];
 
             $this->select->where(
-                "MBRIntersects(coverage, GeomFromText('$bb')) OR
-                is_wms = 1" // Always match WMS layers.
+                "MBRIntersects(coverage, GeomFromText('?')) OR
+                is_wms = 1", $bb // Always match WMS layers.
             );
 
             // Omit records with empty coverages.

commit a721864eda6f65fb7b3f33af188d0eaf66edd3ed

Corrects SELECT statement syntax.
Bug introduced in 9a30214. Refs #470.

models/NeatlineRecordTable.php
@@ -163,7 +163,7 @@ class NeatlineRecordTable extends Neatline_Table_Expandable
             $bb = $this->params['extent'];
 
             $this->select->where(
-                "MBRIntersects(coverage, GeomFromText('?')) OR
+                "MBRIntersects(coverage, GeomFromText(?)) OR
                 is_wms = 1", $bb // Always match WMS layers.
             );
 